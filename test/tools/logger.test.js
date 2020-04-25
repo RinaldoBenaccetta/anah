f4a9d9d116e543ce31cf368f0ed104f121488ee5
rinaldo <rinaldobenaccetta@hotmail.com>
2020-04-25 16:03:52 +0200

    test: add test for logger

---
+++ test/tools/logger.test.js
@@ -0,0 +1,19 @@
+const log = require('../../tools/logger')
+
+describe('logger', () => {
+  test('Warning with valid string, log a warning', () => {
+    console.warn = jest.fn()
+    log.warning('alert')
+    expect(console.warn).toHaveBeenCalled()
+  })
+  test('Info with valid string, log info', () => {
+    console.info = jest.fn()
+    log.info('alert')
+    expect(console.info).toHaveBeenCalled()
+  })
+  test('Error with valid string, log error', () => {
+    console.error = jest.fn()
+    log.error('alert')
+    expect(console.error).toHaveBeenCalled()
+  })
+})
