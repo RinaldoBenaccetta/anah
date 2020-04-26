c0979ba8ce948850d299889ddad0aa1d21bdbe1b
rinaldo <rinaldobenaccetta@hotmail.com>
2020-04-26 17:57:52 +0200

    refactor: use simplier read content system
    and add a warning when multiple files with same names

--- test/tools/getContent.test.js
+++ test/tools/getContent.test.js
@@ -5,6 +5,8 @@ const expected = require('../expected/partials/getContent.expected')
 // ! folders must be from root of the app and not from the test himself
 const fixtureFolder = './test/fixtures/partials/**/*.{html,md,hbs}'
 
+// TODO : test warning log for multiple files with same name.
+
 describe('getContent', () => {
   test('with valid folder return the content of files', async () => {
     const content = await getContent(fixtureFolder)
--- tools/getContent.js
+++ tools/getContent.js
@@ -1,28 +1,100 @@
-const read = require('file-reader')
-const vinylToString = require('vinyl-contents-tostring')
+const fse = require('fs-extra')
+const globby = require('globby')
+const getFileName = require('./getFileName')
+const log = require('../tools/logger')
 
 /**
- * Return an object with content of the specified files in string format.
- *
- * @param {string} files
+ * @description
+ * Array that contain already parsed files.
  */
-module.exports = async (files) => {
-  const content = await read(files) // this output a stream
-  const output = await toString(content)
+// eslint-disable-next-line prefer-const
+let alreadyParsed = []
+
+/**
+ * @function getContent
+ *
+ * @description
+ * Return an object with content of the specified files in string format.
+ * If there is multiple files with same names in folders and sub-folders,
+ * they will overwrite each others.
+ *
+ * @param {string} folder The folder to gets content from.
+ * @param {string} rawFolder The raw provide folder.
+ *
+ * @return {object.<string, string>} An object with the contents.
+ */
+module.exports = async (folder, rawFolder) => {
+  const glob = await globby(folder)
+
+  const output = {}
+
+  for (const file of glob) {
+    // add content from file to the returned object.
+    // if undefined , nothing is pushed.
+    Object.assign(output, await getContent(file, rawFolder))
+  }
 
   return output
 }
 
 /**
- * Return an object with stream values converted to strings.
+ * @function getContent
  *
- * @param {object} content
+ * @description
+ * Read an object with a key
+ * that is the name of the file (without path and extension)
+ * and the content of the file
+ *
+ * @param {string} file {description}
+ * @param {string} rawFolder The raw provide folder.
+ *
+ * @return {object.<string, string>}
+ * The content with the key that is the name of file.
  */
-const toString = async (content) => {
-  Object.keys(content).forEach(async (key) => {
-    // this convert streams to string
-    content[key] = await vinylToString(content[key])
-  })
+const getContent = async (file, rawFolder) => {
+  const output = {}
+  const fileName = getFileName(file)
 
-  return content
+  isAlreadyParsed(fileName, rawFolder)
+  const content = await fse.readFile(file, 'utf8')
+
+  output[fileName] = content
+
+  return output
+}
+
+/**
+ * @function isAlreadyParsed
+ *
+ * @description
+ * Verify if fileName is already parsed.
+ * If already parsed, log a warning message.
+ * If not, add the file name to alreadyParsed array.
+ *
+ * E.g. : data.yml and data.json are both
+ * in the provided folder will log a warning message.
+ *
+ * @param {string} file The name of the file, without path and extension.
+ * @param {string} rawFolder The raw provide folder.
+ */
+const isAlreadyParsed = (file, rawFolder) => {
+  alreadyParsed.includes(file)
+    ? alreadyParsedWarning(file, rawFolder)
+    : alreadyParsed.push(file)
+}
+
+/**
+ * @function alreadyParsedWarning
+ *
+ * @description
+ * Format and log the warning message for datas files with same names.
+ *
+ * @param {string} file The name of the file, without path and extension.
+ * @param {string} rawFolder The raw provide folder.
+ */
+const alreadyParsedWarning = (file, rawFolder) => {
+  log.warning(
+    // eslint-disable-next-line max-len
+    `[${file}] appears more than one time in different format in the provided [${rawFolder}] folder or his sub-folders.\nSome contents could be overwritten.\nTry to avoid to provide multiples content files with same names.\n`
+  )
 }
