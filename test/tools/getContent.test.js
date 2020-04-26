fd7cb9be1a7d4ab8c2722f66aab88819067263bc
rinaldo <rinaldobenaccetta@hotmail.com>
2020-04-26 18:04:55 +0200

    test: add test of warn log for getcontent

--- test/tools/getContent.test.js
+++ test/tools/getContent.test.js
@@ -5,12 +5,13 @@ const expected = require('../expected/partials/getContent.expected')
 // ! folders must be from root of the app and not from the test himself
 const fixtureFolder = './test/fixtures/partials/**/*.{html,md,hbs}'
 
-// TODO : test warning log for multiple files with same name.
-
 describe('getContent', () => {
-  test('with valid folder return the content of files', async () => {
+  test('with valid folder return the content of files, ang get a warning log because two files with same name exists in folder with same names', async () => {
+    // mock console.warn
+    console.warn = jest.fn()
     const content = await getContent(fixtureFolder)
 
     expect(content).toStrictEqual(expected)
+    expect(console.warn).toHaveBeenCalledTimes(2)
   })
 })
