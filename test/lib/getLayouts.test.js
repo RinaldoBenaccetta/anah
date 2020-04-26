5b1ec7335f9548621b535a5bf81d6cae59b00703
rinaldo <rinaldobenaccetta@hotmail.com>
2020-04-26 16:25:52 +0200

    feature: add error when layouts folder contain no layouts

--- test/lib/getLayouts.test.js
+++ test/lib/getLayouts.test.js
@@ -2,42 +2,58 @@ const getLayouts = require('../../lib/getLayouts')
 
 const expected = require('../expected/lib/getLayouts.expected')
 
-// // ! folders must be from root of the app and not from the test himself
-// const fixtureFolder = './test/fixtures/partials/**/*.{html,md,hbs}'
-
 describe('getLayouts', () => {
+  // mock console.warn
+  console.warn = jest.fn()
+
   test('With a path with at least a default layout, return the layouts.', async () => {
-    const options = {
+    const rawOptions = {
+      // ! folders must be from root of the app and not from the test himself.
+      // TODO : add markdown support.
+      layouts: './test/fixtures/layouts/'
+    }
+
+    const folders = {
       // ! folders must be from root of the app and not from the test himself.
       // TODO : add markdown support.
       layouts: './test/fixtures/layouts/**/*.{html,hbs}'
     }
-    const layouts = await getLayouts(options)
+    const layouts = await getLayouts(folders, rawOptions)
 
     expect(layouts).toStrictEqual(expected.layouts)
   })
 
   test('With a folder that not contain default template, log a warning.', async () => {
-    // mock console.warn
-    console.warn = jest.fn()
+    const rawOptions = {
+      // ! folders must be from root of the app and not from the test himself.
+      // TODO : add markdown support.
+      layouts: './test/fixtures/layouts/without-default/'
+    }
 
-    const options = {
+    const folders = {
       // ! folders must be from root of the app and not from the test himself.
       // TODO : add markdown support.
       layouts: './test/fixtures/layouts/without-default/**/*.{html,hbs}'
     }
-    const layouts = await getLayouts(options)
+
+    await getLayouts(folders, rawOptions)
+
+    expect(console.warn).toHaveBeenCalled()
   })
 
-  test('With a folder that contain no template, throw an error.', async () => {
-    // mock console.warn
-    console.warn = jest.fn()
+  test('With a folder that contain no template, throw an error.', () => {
+    const rawOptions = {
+      // ! folders must be from root of the app and not from the test himself.
+      // TODO : add markdown support.
+      layouts: './test/fixtures/layouts/empty/'
+    }
 
-    const options = {
+    const folders = {
       // ! folders must be from root of the app and not from the test himself.
       // TODO : add markdown support.
       layouts: './test/fixtures/layouts/empty/**/*.{html,hbs}'
     }
-    const layouts = await getLayouts(options)
+
+    return expect(getLayouts(folders, rawOptions)).rejects.toThrow()
   })
 })
