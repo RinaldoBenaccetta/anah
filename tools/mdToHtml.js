247d7ce38385de7561432306c725539e3404de59
rinaldo <rinaldobenaccetta@hotmail.com>
2020-04-28 12:53:10 +0200

    doc: fill doc

--- tools/mdToHtml.js
+++ tools/mdToHtml.js
@@ -2,6 +2,16 @@ const showdown = require('showdown')
 
 // TODO :  Add showdown options. At least for the flavor of markdown.
 
+/**
+ * @function mdToHtml
+ *
+ * @description
+ * Convert the provided markdown string to HTML.
+ *
+ * @param  {type} markdown The markdown string.
+ *
+ * @return {type} The string converted to HTML.
+ */
 module.exports = (markdown) => {
   const converter = new showdown.Converter()
 
