62d748672af11f28283d4bb6d2ec6d183e836014
rinaldo <rinaldobenaccetta@hotmail.com>
2020-05-01 15:40:58 +0200

    test: simplify fixture

--- test/fixtures/lib/registerHelpers/subfolder/tobold.js
+++ test/fixtures/lib/registerHelpers/subfolder/tobold.js
@@ -1,6 +1,3 @@
 module.exports = function (options) {
-  // eslint-disable-next-line no-undef
-  return new Handlebars.SafeString(
-    '<div class="bold">' + options.fn(this) + '</div>'
-  )
+  // do something
 }
