363844454b7d2d8330ba79b6d05bca493b7062e7
rinaldo <rinaldobenaccetta@hotmail.com>
2020-05-03 21:00:00 +0200

    refactor: datas are named by the name of their files

--- test/expected/lib/getDatas.expected.js
+++ test/expected/lib/getDatas.expected.js
@@ -1,41 +1,89 @@
+// module.exports = {
+//   datasYmlAndJson: {
+//     dogs: [
+//       {
+//         age: 3,
+//         name: 'Sam'
+//       },
+//       {
+//         age: 5,
+//         name: 'Rex'
+//       }
+//     ],
+//     main: [
+//       {
+//         page: 'index',
+//         target: '_self',
+//         text: 'Home'
+//       },
+//       {
+//         page: 'example',
+//         target: '_self',
+//         text: 'Example'
+//       }
+//     ],
+//     persons: [
+//       {
+//         age: 25,
+//         name: 'Joe'
+//       },
+//       {
+//         age: 15,
+//         name: 'Frank'
+//       },
+//       {
+//         age: 45,
+//         name: 'John'
+//       }
+//     ],
+//     title: 'awesome title'
+//   }
+// }
+
 module.exports = {
   datasYmlAndJson: {
-    dogs: [
-      {
-        age: 3,
-        name: 'Sam'
-      },
-      {
-        age: 5,
-        name: 'Rex'
-      }
-    ],
-    main: [
-      {
-        page: 'index',
-        target: '_self',
-        text: 'Home'
-      },
-      {
-        page: 'example',
-        target: '_self',
-        text: 'Example'
-      }
-    ],
-    persons: [
-      {
-        age: 25,
-        name: 'Joe'
-      },
-      {
-        age: 15,
-        name: 'Frank'
-      },
-      {
-        age: 45,
-        name: 'John'
-      }
-    ],
-    title: 'awesome title'
+    names: {
+      persons: [
+        {
+          age: 25,
+          name: 'Joe'
+        },
+        {
+          age: 15,
+          name: 'Frank'
+        },
+        {
+          age: 45,
+          name: 'John'
+        }
+      ],
+      dogs: [
+        {
+          age: 3,
+          name: 'Sam'
+        },
+        {
+          age: 5,
+          name: 'Rex'
+        }
+      ]
+    },
+    menu: {
+      main: [
+        {
+          page: 'index',
+          target: '_self',
+          text: 'Home'
+        },
+        {
+          page: 'example',
+          target: '_self',
+          text: 'Example'
+        }
+      ]
+    },
+    datas: {
+      title: 'awesome title'
+    }
   }
 }
