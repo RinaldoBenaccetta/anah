8a8c3e5665f646637e3804eaf5d49b9130b92d95
rinaldo <rinaldobenaccetta@hotmail.com>
2020-05-04 17:41:37 +0200

    refactor: call module directly instead of by build

--- test/lib/anah.test.js
+++ test/lib/anah.test.js
@@ -11,13 +11,13 @@ fse.outputFile = jest.fn()
 
 describe('anah', () => {
   test('With valid options and 6 pages on the provided pages options, save 6 pages.', async () => {
-    await anah.build(validOptions)
+    await anah(validOptions)
 
     expect(fse.outputFile).toHaveBeenCalledTimes(expected.pageNumber)
   })
 
   test('With valid options and 6 pages on the provided pages options, render 6 pages and return them in an object.', async () => {
-    const build = await anah.build(validOptions)
+    const build = await anah(validOptions)
 
     expect(build).toStrictEqual(expected.output)
   })
