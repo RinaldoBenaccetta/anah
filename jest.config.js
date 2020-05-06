eb517591dd0d2497d30458eac63dbc20f5e52946
rinaldo <rinaldobenaccetta@hotmail.com>
2020-05-06 12:01:16 +0200

    test: improve test coverage

--- jest.config.js
+++ jest.config.js
@@ -20,13 +20,20 @@ module.exports = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    '!**/*.experiment.js',
+    '**/*.js',
+    '!**/test/fixtures/**/*.js',
+    '!**/test/coverage/**/*.js',
+    '!/node_modules/',
+    '!**/*.config.js'
+  ],
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: 'coverage',
+  coverageDirectory: 'test/coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
